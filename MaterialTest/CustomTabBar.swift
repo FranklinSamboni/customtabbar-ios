5230342d9f46fd881e2092652238aa1bb1840a11
Franklin Samboni Castillo <sambonifranklin@gmail.com>
2019-09-03 22:00:20 -0500

    first commit

---
+++ MaterialTest/CustomTabBar.swift
@@ -0,0 +1,9 @@
+//
+//  CustomTabBar.swift
+//  MaterialTest
+//
+//  Created by Franklin Samboni on 9/3/19.
+//  Copyright © 2019 Franklin Samboni. All rights reserved.
+//
+
+import Foundation
---
+++ MaterialTest/CustomTabBarItemCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarItemCollectionViewCell.swift
+//  MaterialTest
+//
+//  Created by Franklin Samboni on 9/3/19.
+//  Copyright © 2019 Franklin Samboni. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
